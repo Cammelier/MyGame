18bf280c3bc66345824ad04c189720e9d56b9b3c
Cammelier <car.iengo@stud.uniroma3.it>
2024-07-11 16:37:05 +0200

    jump1

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float jumpForce = 4f;
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayerJump()
+    {
+        if (Input.touches()
+        {
+            rb.velocity = Vector2.up;
+        }
     }
 }
