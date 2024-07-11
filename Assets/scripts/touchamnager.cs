dc369a900c99c15026aa900a7dd371396159839e
Cammelier <car.iengo@stud.uniroma3.it>
2024-07-11 17:09:08 +0200

    script

--- Assets/scripts/touchamnager.cs
+++ Assets/scripts/touchamnager.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
+using System.Runtime.CompilerServices;
 
 public class touchamnager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject player;
+
+    private PlayerInput playerinput;
+
+    private InputAction touchPositionAction;
+    private InputAction touchPressAction;
+
+
+    private void Awake()
     {
-        
+       
+ 
+        playerinput = GetComponent<PlayerInput>();
+         touchPressAction = playerinput.actions["TouchPress"];
+        touchPositionAction = playerinput.actions["TouchPosition"];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+         
+        touchPressAction.performed += TouchPressed; 
+    }
+
+    private void OnDisable()
+    {
+        touchPressAction.performed -= TouchPressed;
+    }
+
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+       Vector3 position = Camera.main.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
+        position.z = player.transform.position.z;
+        player.transform.position = position;
+    }
+
+    private void Update()
+    {
+        touchPositionAction.ReadValue<Vector2>();
+        if (touchPositionAction.WasPerformedThisFrame())
+        {
+            Vector3 position = Camera.main.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
+            position.z = player.transform.position.z;
+            player.transform.position = position;
+        }
     }
 }
+
