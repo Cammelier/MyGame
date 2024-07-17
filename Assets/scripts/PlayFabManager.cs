4c01bbb9c44081f54151d55152a2fc9384e808f2
Cammelier <car.iengo@stud.uniroma3.it>
2024-07-17 12:42:41 +0200

    inizio login

---
+++ Assets/scripts/PlayFabManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class PlayFabManager : MonoBehaviour
+{
+    [Header("UI")]
+    public Text messageText;
+    public InputField emailInput;
+    public InputField passwordInput;
+
+    public void RegisterButton()
+    {
+        var request = new RegisterPlayFabUserRequest()
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+    }
+
+    void OnRegisterSuccess(RegisterPlayFabUserRequest result)
+    {
+        messageText.text = "registered and logged in";
+
+    }
+}
